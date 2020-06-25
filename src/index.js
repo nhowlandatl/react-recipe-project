cdcdc8943d991b521b68de253ba62ea7e21b9436
Nick Howland <nhowland@outlook.com>
2020-06-25 19:00:34 -0400

    added project to repo

--- src/index.js
+++ src/index.js
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Recipe from './components/Recipe';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'; 
+
+// Redux store setup
+const initialState = {
+  count: 0
+}
+
+// Write functions for your reducer here
+function reducer(state = initialState, action) {
+  // blah blah
+}
+
+// Reducer store
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>  
+      <Recipe/>
+    </Provider>  
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +33,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
