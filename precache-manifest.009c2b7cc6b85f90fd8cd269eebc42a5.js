ed4c2e94ed81298c34e085bfad1bf93fcdfd24ff
Nick Howland <nhowland@outlook.com>
2020-06-30 14:15:13 -0400

    Updates

---
+++ precache-manifest.009c2b7cc6b85f90fd8cd269eebc42a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "141d0e92bdeff4a8257ee940420a566e",
+    "url": "/react-recipe-project/index.html"
+  },
+  {
+    "revision": "3f64b69f6fa51da42f02",
+    "url": "/react-recipe-project/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "adb5b241e7c9d7bafee9",
+    "url": "/react-recipe-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3f64b69f6fa51da42f02",
+    "url": "/react-recipe-project/static/js/2.4f5538a6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-recipe-project/static/js/2.4f5538a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb5b241e7c9d7bafee9",
+    "url": "/react-recipe-project/static/js/main.71364a24.chunk.js"
+  },
+  {
+    "revision": "7b0d155b2dab35de03b2",
+    "url": "/react-recipe-project/static/js/runtime-main.90f970cb.js"
+  }
+]);
