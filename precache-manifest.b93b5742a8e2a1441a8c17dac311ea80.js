97d8a310715dd66daa4c92da90d2c81c2388b28b
Nick Howland <nhowland@outlook.com>
2020-07-02 23:57:36 -0400

    Updates

---
+++ precache-manifest.b93b5742a8e2a1441a8c17dac311ea80.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6a0760ec07aa42af48c2f402162853",
+    "url": "/react-recipe-project/index.html"
+  },
+  {
+    "revision": "dcc5a4b164ea74531aff",
+    "url": "/react-recipe-project/static/css/2.f3958377.chunk.css"
+  },
+  {
+    "revision": "15ef7b7365ee0eaec02d",
+    "url": "/react-recipe-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dcc5a4b164ea74531aff",
+    "url": "/react-recipe-project/static/js/2.3c559ac3.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/react-recipe-project/static/js/2.3c559ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ef7b7365ee0eaec02d",
+    "url": "/react-recipe-project/static/js/main.360a7398.chunk.js"
+  },
+  {
+    "revision": "7b0d155b2dab35de03b2",
+    "url": "/react-recipe-project/static/js/runtime-main.90f970cb.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/react-recipe-project/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/react-recipe-project/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/react-recipe-project/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/react-recipe-project/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/react-recipe-project/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/react-recipe-project/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/react-recipe-project/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/react-recipe-project/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/react-recipe-project/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/react-recipe-project/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/react-recipe-project/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/react-recipe-project/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/react-recipe-project/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/react-recipe-project/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/react-recipe-project/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/react-recipe-project/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/react-recipe-project/static/media/fa-solid-900.f3a7d3b5.woff"
+  }
+]);
